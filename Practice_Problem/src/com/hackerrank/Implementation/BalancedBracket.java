724da19592bd4d5933030c92d08eda54af527376
Vipul_Anky <Vipul_Anky@192.168.1.5>
2017-04-30 13:18:52 +0530

    Solved problem from hackerearth

---
+++ Practice_Problem/src/com/hackerrank/Implementation/BalancedBracket.java
@@ -0,0 +1,52 @@
+package com.hackerrank.Implementation;
+
+/**
+ * 
+ * Question2Max. Marks 100.00
+Balance Brackets
+Munchi is a newbie in programming, so he is practicing some standard questions. He came across the problem of balanced brackets. But Munchi's friend BlackHammer tried to bully him by slightly changing the problem.
+
+He took a balanced bracket string and removed only one bracket from it. Now, he asks Munchi that how many different balanced bracket string can he make after inserting the removed bracket back at any place possible.
+
+Munchi wants your help.
+
+Note: Only one single bracket is removed from an otherwise balanced string of brackets.
+
+Input
+
+First line contains the string with only "(" and ")".
+Next line contains either 1 or 2.(1 means "(" is removed, 2 means ")" is removed)
+Output
+
+Output total number of balanced bracket strings.
+
+Constraints
+
+1≤Length of input string≤105
+
+Sample Input
+	(()
+	2
+Sample Output
+	2
+
+Explanation
+	2 possible string are:
+	()() and (())
+
+Time Limit: 1.0 sec(s) for each input file
+Memory Limit: 256 MB
+Source Limit: 1024 KB
+Marking Scheme: Marks are awarded if any testcase passes
+Allowed Languages: Java, Java 8
+ * 
+ * 
+ * @author Vipul_Anky
+ *
+ */
+public class BalancedBracket {
+    public static void main(String[] args){
+	
+	
+    }
+}
